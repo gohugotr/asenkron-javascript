fd94ae2631b0c8527aee9e808fb36111418f8f17
Cengiz YILMAZ <terabilgi@gmail.com>
2022-05-18 20:36:06 +0300

    Callback foksiyonu

--- README.md
+++ README.md
@@ -22,3 +22,39 @@ git remote add origin https://github.com/gohugotr/asenkronjavascript.git
 git branch -M master
 git push -u origin master
 ```
+### Callback fonksiyonu
+
+`Fonksiyon içerisinde parametre olarak gönderilen fonksiyonlara callback fonksiyonlar denir.`
+
+```js script
+const getUsers = (callback) => {
+    const xhr = new XMLHttpRequest()
+
+    xhr.addEventListener('readystatechange', () => {
+      // console.log(xhr,xhr.readyState);
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        //console.log(xhr.responseText)
+        callback(undefined, xhr.responseText)
+        // Standart kullanım ilk paramtre hata, 2. parametrede doğru verinin paramtresi şeklinde
+            // İlk parametre burada hata olmayacağından 'undefined' oluyor.
+            // 2. parametrede de veri içeren 'xhr.responseText' oluyor.
+      } else if (xhr.readyState == 4) {
+        //console.log('Veri erişilemedi');
+        callback('Veriye erişilemedi', undefined)
+            // İlk parametre burada hata olacağından hata mesajı 'Veriye erişilemedi' görüntülenir.
+            // 2. parametrede de veri olmadığından 'undefined' girilir.
+      }
+    })
+
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+    xhr.send();
+}
+
+getUsers((err,data)=>{
+    if(err){
+        console.log(err);
+    } else {
+        console.log(data);
+    }
+});
+```
--- httpRequest.js
+++ httpRequest.js
@@ -1,14 +1,30 @@
-const xhr = new XMLHttpRequest();
+const getUsers = (callback) => {
+    const xhr = new XMLHttpRequest()
 
-xhr.addEventListener('readystatechange', ()=>{
-   // console.log(xhr,xhr.readyState);
-    if (xhr.readyState==4 && xhr.status==200){
-        console.log(xhr.responseText);
-    } else if (xhr.readyState==4) {
-        console.log('Veri Getirilemedi');
+    xhr.addEventListener('readystatechange', () => {
+      // console.log(xhr,xhr.readyState);
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        //console.log(xhr.responseText)
+        callback(undefined, xhr.responseText)
+        // Standart kullanım ilk paramtre hata, 2. parametrede doğru verinin paramtresi şeklinde
+            // İlk parametre burada hata olmayacağından 'undefined' oluyor.
+            // 2. parametrede de veri içeren 'xhr.responseText' oluyor.
+      } else if (xhr.readyState == 4) {
+        //console.log('Veri erişilemedi');
+        callback('Veriye erişilemedi', undefined)
+            // İlk parametre burada hata olacağından hata mesajı 'Veriye erişilemedi' görüntülenir.
+            // 2. parametrede de veri olmadığından 'undefined' girilir.
+      }
+    })
+
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+    xhr.send();
+}
+
+getUsers((err,data)=>{
+    if(err){
+        console.log(err);
+    } else {
+        console.log(data);
     }
 });
-
-xhr.open('GET', 'https://jsonplaceholder.typicode.com/userss')
-xhr.send();
-
