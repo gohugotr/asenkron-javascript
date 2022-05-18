3c328825a117029e6525400c71fc2c51d3ff8244
Cengiz YILMAZ <terabilgi@gmail.com>
2022-05-18 20:16:34 +0300

    HttpRequest ile GET işlemi

--- httpRequest.js
+++ httpRequest.js
@@ -2,11 +2,13 @@ const xhr = new XMLHttpRequest();
 
 xhr.addEventListener('readystatechange', ()=>{
    // console.log(xhr,xhr.readyState);
-    if (xhr.readyState==4){
+    if (xhr.readyState==4 && xhr.status==200){
         console.log(xhr.responseText);
+    } else if (xhr.readyState==4) {
+        console.log('Veri Getirilemedi');
     }
 });
 
-xhr.open('GET', 'https://jsonplaceholder.typicode.com/users')
+xhr.open('GET', 'https://jsonplaceholder.typicode.com/userss')
 xhr.send();
 
