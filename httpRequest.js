ad3bcd2ea8c0b99fc0e06ff02513b3900dfe752a
Cengiz YILMAZ <terabilgi@gmail.com>
2022-05-21 11:19:55 +0300

    Promis yapısı

--- httpRequest.js
+++ httpRequest.js
@@ -1,49 +1,66 @@
-const getUsers = (url, callback) => {
-    const xhr = new XMLHttpRequest()
-
-    xhr.addEventListener('readystatechange', () => {
-      // console.log(xhr,xhr.readyState);
-      if (xhr.readyState == 4 && xhr.status == 200) {
-        //console.log(xhr.responseText)
-        const data = JSON.parse(xhr.responseText); // Veriler JSON olarak döndürüldü.
-        callback(undefined, data);
-        // Standart kullanım ilk parametre hata, 2. parametrede doğru verinin parametresi şeklinde
-            // İlk parametre burada hata olmayacağından 'undefined' oluyor.
-            // 2. parametrede de veri içeren 'xhr.responseText' oluyor.
-      } else if (xhr.readyState == 4) {
-        //console.log('Veri erişilemedi');
-        callback('Veriye erişilemedi', undefined);
-            // İlk parametre burada hata olacağından hata mesajı 'Veriye erişilemedi' görüntülenir.
-            // 2. parametrede de veri olmadığından 'undefined' girilir.
-      }
-    })
-
-    xhr.open('GET', url)
-    xhr.send();
+const veriGetir = () => {
+  return new Promise((resolve, reject) => {
+    // resolve('Veri başarıyla getirildi');
+    // Veri başarıyla getirildiyse reject çalışmaz.
+    reject('Veri hatası')
+    // Veri hatalı geldiyse, burası çalışır.
+  })
 }
 
-const url = 'https://jsonplaceholder.typicode.com/users'
+veriGetir()
+  .then((data) => {
+    console.log(data)
+  })
+  .catch((err) => {
+    console.log(err)
+  })
 
-getUsers('users/bidb.json',(err,data)=>{
-    if(err){
-        console.log(err);
-    } else {
+// const getUsers = (url, callback) => {
+//     const xhr = new XMLHttpRequest()
 
-        console.log('bidb.json verileri:/n', data);
+//     xhr.addEventListener('readystatechange', () => {
+//       // console.log(xhr,xhr.readyState);
+//       if (xhr.readyState == 4 && xhr.status == 200) {
+//         //console.log(xhr.responseText)
+//         const data = JSON.parse(xhr.responseText); // Veriler JSON olarak döndürüldü.
+//         callback(undefined, data);
+//         // Standart kullanım ilk parametre hata, 2. parametrede doğru verinin parametresi şeklinde
+//             // İlk parametre burada hata olmayacağından 'undefined' oluyor.
+//             // 2. parametrede de veri içeren 'xhr.responseText' oluyor.
+//       } else if (xhr.readyState == 4) {
+//         //console.log('Veri erişilemedi');
+//         callback('Veriye erişilemedi', undefined);
+//             // İlk parametre burada hata olacağından hata mesajı 'Veriye erişilemedi' görüntülenir.
+//             // 2. parametrede de veri olmadığından 'undefined' girilir.
+//       }
+//     })
 
-        getUsers('users/muhasebe.json', (err, data) => {
-          if (err) {
-            console.log(err)
-          } else {
-            console.log('muhasebe.json verileri:/n', data);
-            getUsers(url, (err, data) => {
-              if (err) {
-                console.log(err)
-              } else {
-                console.log('json placeholder sitesi users verileri:/n', data);
-              }
-            })
-          }
-        })
-    }
-});
+//     xhr.open('GET', url)
+//     xhr.send();
+// }
+
+// const url = 'https://jsonplaceholder.typicode.com/users'
+
+// getUsers('users/bidb.json',(err,data)=>{
+//     if(err){
+//         console.log(err);
+//     } else {
+
+//         console.log('bidb.json verileri:/n', data);
+
+//         getUsers('users/muhasebe.json', (err, data) => {
+//           if (err) {
+//             console.log(err)
+//           } else {
+//             console.log('muhasebe.json verileri:/n', data);
+//             getUsers(url, (err, data) => {
+//               if (err) {
+//                 console.log(err)
+//               } else {
+//                 console.log('json placeholder sitesi users verileri:/n', data);
+//               }
+//             })
+//           }
+//         })
+//     }
+// });
