d0a7a287adb4c1b23eef62eb5bb31163bb4b08cd
Cengiz YILMAZ <terabilgi@gmail.com>
2022-05-21 12:22:25 +0300

    Fetch ile veri çekildi.

---
+++ httpRequestwithFetch.js
@@ -0,0 +1,16 @@
+
+const url = 'https://jsonplaceholder.typicode.com/userss';
+
+// ilk adım
+// fetch(url).then((response)=>{
+//     const data = response.json();
+//     console.log(data);
+// });
+
+// ikinci adımla doğrudan veriye erişiyoruz.
+
+fetch(url).then((response)=>{
+    return response.json();
+}).then((data)=>{
+    console.log(data);
+})
