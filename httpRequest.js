e2e5f0a4aec672b43fa5dfbaa54de0b11941ea38
Cengiz YILMAZ <terabilgi@gmail.com>
2022-05-21 11:43:10 +0300

    getUsers fonksiyonu promise yapısıyla yenden yazıldı.

--- httpRequest.js
+++ httpRequest.js
@@ -15,52 +15,44 @@
 //     console.log(err);
 // });
 
-// const getUsers = (url, callback) => {
-//     const xhr = new XMLHttpRequest()
 
-//     xhr.addEventListener('readystatechange', () => {
-//       // console.log(xhr,xhr.readyState);
-//       if (xhr.readyState == 4 && xhr.status == 200) {
-//         //console.log(xhr.responseText)
-//         const data = JSON.parse(xhr.responseText); // Veriler JSON olarak döndürüldü.
-//         callback(undefined, data);
-//         // Standart kullanım ilk parametre hata, 2. parametrede doğru verinin parametresi şeklinde
-//             // İlk parametre burada hata olmayacağından 'undefined' oluyor.
-//             // 2. parametrede de veri içeren 'xhr.responseText' oluyor.
-//       } else if (xhr.readyState == 4) {
-//         //console.log('Veri erişilemedi');
-//         callback('Veriye erişilemedi', undefined);
-//             // İlk parametre burada hata olacağından hata mesajı 'Veriye erişilemedi' görüntülenir.
-//             // 2. parametrede de veri olmadığından 'undefined' girilir.
-//       }
-//     })
+// const getUsers = (url, callback) fonksiyonunu promise yapısına çevirmek için callback'i kaldırıyoruz.
+const getUsers = (url) => {
 
-//     xhr.open('GET', url)
-//     xhr.send();
-// }
+    return new Promise((resolve, reject)=>{
+            const xhr = new XMLHttpRequest()
 
-// const url = 'https://jsonplaceholder.typicode.com/users'
+            xhr.addEventListener('readystatechange', () => {
+              // console.log(xhr,xhr.readyState);
+              if (xhr.readyState == 4 && xhr.status == 200) {
+                //console.log(xhr.responseText)
+                const data = JSON.parse(xhr.responseText) // Veriler JSON olarak döndürüldü.
+                
+                resolve(data); // callback yerine geldi.
+               // callback(undefined, data);
 
-// getUsers('users/bidb.json',(err,data)=>{
-//     if(err){
-//         console.log(err);
-//     } else {
+                // Standart kullanım ilk parametre hata, 2. parametrede doğru verinin parametresi şeklinde
+                // İlk parametre burada hata olmayacağından 'undefined' oluyor.
+                // 2. parametrede de veri içeren 'xhr.responseText' oluyor.
+              } else if (xhr.readyState == 4) {
+                //console.log('Veri erişilemedi');
 
-//         console.log('bidb.json verileri:/n', data);
+                reject('Veriye getirme hatası');
+                //callback('Veriye erişilemedi', undefined);
+                // İlk parametre burada hata olacağından hata mesajı 'Veriye erişilemedi' görüntülenir.
+                // 2. parametrede de veri olmadığından 'undefined' girilir.
+              }
+            })
+          xhr.open('GET', url);
+          xhr.send();
+    })
+}
 
-//         getUsers('users/muhasebe.json', (err, data) => {
-//           if (err) {
-//             console.log(err)
-//           } else {
-//             console.log('muhasebe.json verileri:/n', data);
-//             getUsers(url, (err, data) => {
-//               if (err) {
-//                 console.log(err)
-//               } else {
-//                 console.log('json placeholder sitesi users verileri:/n', data);
-//               }
-//             })
-//           }
-//         })
-//     }
-// });
+const url = 'https://jsonplaceholder.typicode.com/users';
+
+getUsers(url).then((data)=>{
+    console.log(data);
+}).catch((err)=>{
+    console.log(err);
+});
+
